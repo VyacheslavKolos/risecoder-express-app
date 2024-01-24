5a9141c1ddde020f36fda6ed75921cae6f938e33
ViacheslavKolosInox <viacheslav.kolos@inoxoft.com>
2024-01-24 15:52:19 +0200

    based configured project

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const app=express();
+const app = express();
 
 const userRouter = require('./api/user/router');
 
 app.use('/users', userRouter);
 
-app.listen(3001, ()=>{
-    console.log('Listen 3001')
-})
+app.listen(3001, () => {
+  console.log('Listen 3001');
+});
